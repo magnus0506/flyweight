7ba5b29145388edd97a739565c94c1add1f9ef0b
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-04-21 16:59:58 +0200

    cleaned up code

--- src/FlyWeightTest.java
+++ src/FlyWeightTest.java
@@ -1,38 +1,31 @@
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class FlyWeightTest {
 
     public static void main(String[] args) {
-
         MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
         MemoryUsage beforeHeapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
 
-        newEngineerFactory();
+        newEngineerObjects();
+//        newEngineerFactory();
 
         MemoryUsage afterHeapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
         long consumed = afterHeapMemoryUsage.getUsed() - beforeHeapMemoryUsage.getUsed();
-        System.out.println("brugt memory efter metode: " + (consumed) + "");
+        System.out.println("used memory:" + (consumed / 1024) + "kb");
 
-        newEngineerObjects();
-
-        afterHeapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
-        consumed = afterHeapMemoryUsage.getUsed() - beforeHeapMemoryUsage.getUsed();
-        System.out.println("brugt memory efter metode: " + (consumed) + "");
 
     }
+
     private static void newEngineerFactory() {
         System.out.println("New objects if it doesn't exist (Factory method)\n");
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 99_000_000; i++) {
             Engineer engineer = (Engineer) JobFactory.getEngineer(getRandomEngineerType());
             engineer.setAge(randomInt(18, 60));
             engineer.setId(randomInt(1, 10));
-//            engineer.printJob();
-//            System.out.println(engineer.hashCode());
+//            printVariablesAndHashCode(engineer);
         }
         long endTime = System.currentTimeMillis();
         System.out.println("Number of objects created: " + Engineer.noOfObjects);
@@ -42,26 +35,21 @@ public class FlyWeightTest {
     private static void newEngineerObjects() {
         System.out.println("New objects for every person\n");
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 99_000_000; i++) {
             Engineer engineer = new Engineer(getRandomEngineerType());
             engineer.setAge(randomInt(18, 60));
             engineer.setId(randomInt(1, 10));
-//            engineer.printJob();
-//            System.out.println(engineer.hashCode());
+//            printVariablesAndHashCode(engineer);
         }
-
         long endTime = System.currentTimeMillis();
         System.out.println("Number of objects created: " + Engineer.noOfObjects);
         System.out.println("Time it took: " + (endTime - startTime) + "ms");
     }
 
-    private static final String[] engineerTypes =
-            {
-                    "Software engineer",
-                    "Database engineer",
-                    "Chemical engineer",
-                    "Windmill engineer",
-                    "Network engineer"};
+    private static void printVariablesAndHashCode(Engineer engineer) {
+        engineer.printJob();
+        System.out.println(engineer.hashCode());
+    }
 
     public static int randomInt(int min, int max) {
         int range = max - min + 1;
@@ -71,5 +59,14 @@ public class FlyWeightTest {
     private static String getRandomEngineerType() {
         return engineerTypes[(int) (Math.random() * engineerTypes.length)];
     }
+
+    private static final String[] engineerTypes =
+            {
+                    "Software engineer",
+                    "Database engineer",
+                    "Chemical engineer",
+                    "Windmill engineer",
+                    "Network engineer"
+            };
 }
 
